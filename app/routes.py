2647bbf604ae2b7957eef9ddd252516ac2edd812
Admin User <admin@MacBook-Pro.local>
2025-07-06 17:08:15 +0530

    feat: templates for webpages

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username':'Hari'}
+    posts = [
+        {
+            'author': {'username':'Tom Brady'},
+            'body': 'Sports & spirit'
+        },
+        {
+            'author': {'username':'Virat Kohli'},
+            'body': 'A Masterclass in Cricket, Country & Courtship'
+        }
+    ]
+    return render_template('index.html',title='Home',user=user, posts = posts)
