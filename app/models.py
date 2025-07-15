025d2f2be19ada4337bc1e60a3425ef3b0605c25
Admin User <admin@MacBook-Pro.local>
2025-07-15 13:04:57 +0530

    posts table

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from enum import unique
+from datetime import datetime
 
 from app import db
 
@@ -10,3 +10,12 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, default = datetime.utcnow)
+
+    def __repr__(self):
+        return '<User {} Post {}>'.format(self.user_id,self.body)
