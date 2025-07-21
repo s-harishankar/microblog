425784de1cf91daf0349ee75fc72fd0f0ff31cb5
Admin User <admin@MacBook-Pro.local>
2025-07-21 22:58:51 +0530

    feat: user login

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,14 @@
 from app import app
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
+from urllib.parse import urlparse
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username':'Hari'}
     posts = [
         {
             'author': {'username':'Tom Brady'},
@@ -16,12 +19,26 @@ def index():
             'body': 'A Masterclass in Cricket, Country & Courtship'
         }
     ]
-    return render_template('index.html',title='Home',user=user, posts = posts)
+    return render_template('index.html',title='Home Page', posts = posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username, form.remember_me))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid Username or Password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlparse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html',title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
